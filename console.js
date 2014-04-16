1af5048e7c05920f6fecd54e94d3dcfa447d718e
Gwenaël Pluchon <accounts@gwenp.fr>
2014-04-16 22:06:49 +0200

    add mute tags feature, and message filtering feature

--- console.js
+++ console.js
@@ -2,6 +2,8 @@ Console = Ember.Object.extend({
 	baseConsole: undefined,
 	author: undefined,
 	tags: [],
+	mutedTags: [],
+	filter: undefined,
 
 	log: function() {
 		if(this.baseConsole !== undefined) {
@@ -14,9 +16,22 @@ Console = Ember.Object.extend({
 
 			if(this.tags !== undefined) {
 				for (var i = this.tags.length - 1; i >= 0 ; i--) {
+					if (this.mutedTags.contains(this.tags[i])) { return; };
 					args.unshift("["+ this.tags[i] +"]");
 				};
 			}
+
+			if(this.filter !== "" && this.filter !== undefined) {
+				for (var i = 0; i < args.length; i++) {
+					if(typeof args[i] === "string") {
+						var regex = new RegExp(this.filter);
+						if(regex.test(args[i])) {
+							return;
+						}
+					}
+				};
+			};
+
 			this.baseConsole.log.apply(this.baseConsole, args);
 		}
 	},
@@ -42,6 +57,23 @@ Console = Ember.Object.extend({
 	setAuthor: function(author) {
 		this.author = author;
 		this.tags.push(this.author);
+	},
+
+	muteTag: function(tag) {
+		this.mutedTags.push(tag);
+	},
+
+	unmuteTag: function(tag) {
+		for (var i = 0; i < this.mutedTags.length; i++) {
+			if(this.mutedTags[i] === tag) {
+				this.mutedTags.splice(i, 1);
+				return;
+			}
+		};
+	},
+
+	filterMessages: function(regex) {
+		this.filter = regex;
 	}
 });
 
@@ -59,4 +91,10 @@ console.log("m3", console);
 console.flushTags();
 console.log("m4", console);
 
+
+console.addTag("t3");
 console.log("Object instantiated");
+
+window.setInterval(function(){
+	console.log("m5");
+},1000);
